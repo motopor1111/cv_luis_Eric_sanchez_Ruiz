e7870804c57cb9d91bfbbeedaec6193cda17e429
motopor1111 <43507810+motopor1111@users.noreply.github.com>
2021-09-25 03:27:39 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -26,3 +26,17 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+const buttons = document.getElementsByTagName("button");
+
+for (const button of buttons) {
+  button.addEventListener('click', () => {
+     var id = button.getAttribute("id");
+    
+     var layerClass = "." + id+ "-layer";
+     var layers = document.querySelectorAll(layerClass);
+     for (const layer of layers) {
+       layer.classList.toggle("active");
+     }
+  });
+}
